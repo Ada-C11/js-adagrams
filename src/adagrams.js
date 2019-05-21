d4846a33d560a2bcbc8bdfaf456bfa24fe6aab39
sjscotton <sj.scotton@gmail.com>
2019-05-21 15:36:15 -0700

    added Adagrams.scoreWord() function, tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,13 +2,17 @@
 
 const Adagrams = {
   letterArray: ["A", "A", "A", "A", "A", "A", "A", "A", "A",
-  "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E",
-  "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H",
-  "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L",
-  "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O",
-  "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R",
-  "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", 
-  "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"],
+    "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E",
+    "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H",
+    "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L",
+    "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O",
+    "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R",
+    "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", 
+    "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"],
+
+  letterScores: {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2,
+    H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10,
+    R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10},
 
   drawLetters() {
     let lettersCopy = [...this.letterArray];
@@ -32,7 +36,13 @@ const Adagrams = {
       handObject[input[i]] -= 1;
     }
     return true;
+  },
 
+  scoreWord(word) {
+    let score = 0;
+    for (let i = 0; i < word.length; i += 1) score += this.letterScores[word[i].toUpperCase()];
+    if (word.length >= 7) score += 8;
+    return score;
   },
 };
 // Adagrams.drawLetters()
