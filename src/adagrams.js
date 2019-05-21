85d71657825acf998749f2b586e56f5486fcd9a3
Jansen <v.jansen.martin@gmail.com>
2019-05-21 16:36:54 -0700

    Add for loop to scoreWord function; some tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,6 +26,35 @@ const letterPool = {
   "Y": 2,
   "Z": 1
 }
+
+const letterScores = {
+  "A": 1,
+  "B": 3,
+  "C": 3,
+  "D": 2,
+  "E": 1,
+  "F": 4,
+  "G": 2,
+  "H": 4,
+  "I": 1,
+  "J": 8,
+  "K": 5,
+  "L": 1,
+  "M": 3,
+  "N": 1,
+  "O": 1,
+  "P": 3,
+  "Q": 10,
+  "R": 1,
+  "S": 1,
+  "T": 1,
+  "U": 1,
+  "V": 4,
+  "W": 4,
+  "X": 8,
+  "Y": 4,
+  "Z": 10
+}
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
@@ -74,6 +103,16 @@ const Adagrams = {
     }
     return result;
   },
+
+  scoreWord(word) {
+
+    let score = 0;
+    for(let i = 0; i < word.length; i += 1) {
+      score += letterScores[word[i]]
+    }
+
+    return score;
+  }
 };
 
 // Do not remove this line or your tests will break!
