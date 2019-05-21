4739869f3893d03a7f91588e4725c75c8b076516
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-21 14:17:04 -0700

    added for loops to push all letters into letterPool

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,54 @@
 const Adagrams = {
   drawLetters() {
+    const letterPool = [];
+
+    for (let i = 0; i < 9; i += 1) {
+      letterPool.push('A');
+      letterPool.push('I');
+    }
+
+    for (let i = 0; i < 2; i += 1) {
+      letterPool.push('B');
+      letterPool.push('C');
+      letterPool.push('F');
+      letterPool.push('H');
+      letterPool.push('M');
+      letterPool.push('P');
+      letterPool.push('V');
+      letterPool.push('W');
+      letterPool.push('Y');
+    }
+
+    for (let i = 0; i < 4; i += 1) {
+      letterPool.push('D');
+      letterPool.push('L');
+      letterPool.push('S');
+      letterPool.push('U');
+    }
+
+    for (let i = 0; i < 12; i += 1) {
+      letterPool.push('E');
+    }
+
+    for (let i = 0; i < 3; i += 1) {
+      letterPool.push('G');
+    }
+  
+    letterPool.push('J');
+    letterPool.push('K');
+    letterPool.push('Q');
+    letterPool.push('X');
+    letterPool.push('Z');
+
+    for (let i = 0; i < 6; i += 1) {
+      letterPool.push('N');
+      letterPool.push('R');
+      letterPool.push('T');
+    }
+
+    for (let i = 0; i < 8; i += 1) {
+      letterPool.push('O');
+    }
     // Implement this method for wave 1
   },
 };
