ac6b98de95e5020f4671968fc0ae1d86b7524673
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-21 15:46:39 -0700

    added drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,25 @@
+const letterArray = ['a','a','a','a','a','a','a','a','a','b','b','c','c','d','d','d','d',
+                  'e','e','e','e','e','e','e','e','e','e','e','e',
+                  'f','f','g','g','g','h','h','i','i','i','i','i','i','i','i','i',
+                  'j','k','l','l','l','l','m','m','n','n','n','n','n','n',
+                  'o','o','o','o','o','o','o','o','p','p','q','r','s','s','s','s',
+                  't','t','t','t','t','t','u','u','u','u','v','v','w','w',
+                  'x','y','y','z']
+
+
 const Adagrams = {
+  
   drawLetters() {
     // Implement this method for wave 1
-  },
+    const shuffled = letterArray.sort(()=> 0.5 - Math.random());
+    let selected = shuffled.slice(0, 10);
+    return selected;
+  }
 };
 
+console.log(Adagrams.drawLetters());
+
+
+
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
