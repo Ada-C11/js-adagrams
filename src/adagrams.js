5505968609105e4541e2854eb3328f2bc26e305b
rosalyn <rosalyn.anuseh@gmail.com>
2019-05-21 15:32:42 -0700

    Created a method to generate aphabet in required quantities

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,46 @@
-const Adagrams = {
-  drawLetters() {
-    // Implement this method for wave 1
-  },
+  const alphaLetters = {
+    A: 9,
+    N: 6,
+    B: 2,
+    O: 8,
+    C: 2,
+    P: 2,
+    D: 4,
+    Q: 1,
+    E: 12,
+    R: 6,
+    F: 2,
+    S: 4,
+    G: 3,
+    T: 6,
+    H: 2,
+    U: 4,
+    I: 9,
+    V: 2,
+    J: 1,
+    W: 2,
+    K: 1,
+    X: 1,
+    L: 4,
+    Y: 2,
+    M: 2,
+    Z: 1
+  };
+
+const createLetters = function createLetters(alphaLetters) {
+  for (let key in alphaLetters) {
+    const result = key.repeat(alphaLetters[key]);
+    console.log(result) 
+  }
 };
 
-// Do not remove this line or your tests will break!
-export default Adagrams;
+createLetters(alphaLetters);
+
+// const Adagrams = {
+//   drawLetters() {
+//     // Implement this method for wave 1
+//   }
+// };
+
+// // Do not remove this line or your tests will break!
+// export default Adagrams;
