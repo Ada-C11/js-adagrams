59a5b1f47b6e13c3f91e125fe42339cc05ebd6b2
Erica Norris <norrise120@gmail.com>
2019-05-21 15:33:29 -0700

    wrote drawLetters method to pass given tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,38 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    //letter counts of all available letters
+    const letterCounts = {
+      'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2,
+      'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2,
+      'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 
+      'Y': 2, 'Z': 1
+    };
+
+    //Create an array of all available letters in respective counts,
+    //this will make sure ensure that e is more likely than z.
+    let letters = []
+    Object.keys(letterCounts).forEach( (letter) => {
+      for (let i = 0; i < letterCounts[letter]; i += 1) {
+        letters.push(letter);
+      }
+    })
+
+    let drawnLetters = []
+    //Logic for drawing the letters
+    for (let i = 0; i < 10; i += 1) {
+      let letter = letters[Math.floor(Math.random() * (letters.length))];
+      drawnLetters.push(letter);
+
+      //This will remove the drawn letter just once from the letters array
+      for (let i = 0; i < letters.length; i += 1) {
+        if (letters[i] === letter) {
+          letters.splice(i, 1);
+          break;
+        }
+      }
+    }
+
+    return drawnLetters
   },
 };
 
