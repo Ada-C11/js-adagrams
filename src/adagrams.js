fcbb10e908357e85323a403c57723a29da571c8e
sjscotton <sj.scotton@gmail.com>
2019-05-21 14:40:50 -0700

    added Adagrams.drawLetters() function, tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,27 @@
+
+
 const Adagrams = {
+  letterArray: ["A", "A", "A", "A", "A", "A", "A", "A", "A",
+  "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E",
+  "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H",
+  "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L",
+  "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O",
+  "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R",
+  "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", 
+  "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"],
+
   drawLetters() {
-    // Implement this method for wave 1
+  let lettersCopy = [...this.letterArray];
+  let hand = [];
+  for (let i = 0; i < 10; i += 1) {
+    let randIndex = Math.floor(Math.random()*lettersCopy.length);
+    hand.push(lettersCopy[randIndex]);
+    lettersCopy.splice(randIndex, 1);
+  }
+  return hand;
   },
 };
-
+// Adagrams.drawLetters()
+// console.log(Adagrams.drawLetters());
 // Do not remove this line or your tests will break!
 export default Adagrams;
