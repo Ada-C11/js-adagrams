dd6c6b424a10b3aca8fad757b1fd0a05d133c7bf
Erica Norris <norrise120@gmail.com>
2019-05-21 16:50:04 -0700

    wrote method highestScoreFrom to pass tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,5 @@
 const Adagrams = {
   drawLetters() {
-    //letter counts of all available letters
     const letterCounts = {
       'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2,
       'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2,
@@ -51,7 +50,6 @@ const Adagrams = {
   },
 
   scoreWord(word) {
-    //letter scores
     const letterScores = {
       'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
       'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
@@ -70,6 +68,28 @@ const Adagrams = {
     }
 
     return score;
+  },
+
+  highestScoreFrom(words) {
+    let winner = {};
+    let highScore = 0;
+    let bestWord = null;
+
+    words.forEach( (word) => {
+      if (this.scoreWord(word) > highScore) {
+        highScore = this.scoreWord(word);
+        bestWord = word;
+      } else if (this.scoreWord(word) === highScore) {
+        if ( (word.length < bestWord.length || word.length === 10) && bestWord.length != 10) {
+          bestWord = word;
+        }
+      }
+    })
+
+    winner['word'] = bestWord
+    winner['score'] = highScore
+
+    return winner
   }
 };
 
