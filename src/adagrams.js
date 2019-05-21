2af7ad66c01abb825088b7e3687d085aa19bd0f0
Shirley Chu <shirley.cchu@gmail.com>
2019-05-21 15:36:16 -0700

    drawLetters function written, tests passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,36 @@
+function shuffle(arra1) {
+  let ctr = arra1.length;
+  let temp;
+  let index;
+
+  // While there are elements in the array
+  while (ctr > 0) {
+// Pick a random index
+      index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+      ctr--;
+// And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+  }
+  return arra1;
+}
+
 const Adagrams = {
+ 
+  letterArray: ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"],
+
   drawLetters() {
-    // Implement this method for wave 1
+    let letterHand = shuffle(this.letterArray).slice(0,10);
+    return letterHand
+  },
+
+  usesAvailiabeLetters () {
+
   },
 };
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
