b77006f71d4b5a84c9812c0def7fc3a0e9196601
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-21 15:56:02 -0700

    wave 3 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -72,6 +72,48 @@ const Adagrams = {
     };
     return true;
   },
+  scoreWord (word) {
+   const scoreChart = {
+      "a":1,
+      "b":3,
+      "c":3,
+      "d":2,
+      "e":1,
+      "f":4,
+      "g":2,
+      "h":4,
+      "i":1,
+      "j":8,
+      "k":5,
+      "l":1,
+      "m":3,
+      "n":1,
+      "o":1,
+      "p":3,
+      "q":10,
+      "r":1,
+      "s":1,
+      "t":1,
+      "u":1,
+      "v":4,
+      "w":4,
+      "x":8,
+      "y":4,
+      "z":10
+    }
+
+    let score = 0
+
+    for (let character of word) {
+      score += scoreChart[character.toLowerCase()];
+    }
+
+    if (word.length >= 7) {
+      score += 8;
+    }
+
+    return score;
+  },
 };
 
 // Do not remove this line or your tests will break!
