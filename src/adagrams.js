ee65bc754efbf77a23282d2248aac24b3ad15352
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-21 19:23:43 -0700

    Write functions: drawLetters, usesAvailableLetters, scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,97 @@
+import { ENGINE_METHOD_DIGESTS } from "constants";
+
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
+    const letterPool = 'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ'.split('');
+    let lettersInHand = [];
+    
+    for (let i = 0; i < 10; i += 1) {
+      lettersInHand.push(letterPool[Math.floor(Math.random() * letterPool.length)]);
+    }
+    return lettersInHand;
+  },
+
+  usesAvailableLetters(input, lettersInHand) {
+    let lettersHash = {};
+
+    lettersInHand.forEach( (letter) => {
+      if (lettersHash.hasOwnProperty(letter)) {
+        lettersHash[letter] += 1;
+      }
+      else {
+        lettersHash[letter] = 1;
+      }
+    })
+    
+    for (let i = 0; i < input.length; i += 1) {
+      if (lettersHash.hasOwnProperty(input[i])) {
+        if (lettersHash[input[i]] <= 0) {
+          return false;
+        }
+        lettersHash[input[i]] -= 1;
+      }
+      else {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  scoreWord(word) {
+    let score = 0
+    if (word.length >= 7) {
+      score += 8;
+    }
+    word.toUpperCase().split('').forEach( (letter) => {
+      switch(letter) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'L':
+        case 'N':
+        case 'R':
+        case 'S':
+        case 'T':
+          score += 1;
+          break;
+        case 'D':
+        case 'G':
+          score += 2;
+          break;
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P':
+          score += 3;
+          break;
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y':
+          score += 4;
+          break;
+        case 'K':
+          score += 5;
+          break;
+        case 'J':
+        case 'X':
+          score += 8;
+          break;
+        case 'Q':
+        case 'Z':
+          score += 10;
+          break;
+      }
+    });
+    return score;
+  },
+
+  highestScoreFrom(words) {
+
   },
 };
 
