367c2407ba76d216ed294aeee31a46901fe59657
Cloudy Lopez <cloudylopez@gmail.com>
2019-05-22 14:09:58 -0700

    added array of letters for pool of letters. added empty array to put randomized letters into with the for loop to return a randomized group of 10 letters that does not affect the original pool of letters.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,20 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
-  },
-};
+    const poolOfLetters = [
+      "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"
+    ];
 
+    let thisHand = poolOfLetters.slice();
+    let myLetters = [];
+    let shuffleLetters = Math.floor(Math.random() * thisHand.length);
+    for (let i = 0; i < 10; i +=1) {
+      myLetters.push(thisHand[shuffleLetters]);
+      thisHand.splice( shuffleLetters, 1 );
+    }
+    return myLetters;
+  //  console.log(myLetters);
+  },
+} 
+console.log(Adagrams.drawLetters());
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
