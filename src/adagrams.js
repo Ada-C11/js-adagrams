ed4e0de554cc0b8ce36784070909ff4103cea780
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-22 14:49:17 -0700

    fixed drawLetters function to make sure letters are being removed from bank

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,22 +1,27 @@
 class Adagrams {
   static drawLetters() {
-    const letterPool = [];
-
     const adagramsCount = {
        'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
     };
+
+    const letterPool = [];
 
     for (let key in adagramsCount) {
       for (let i = 0; i < adagramsCount[key]; i++) {
         letterPool.push(key);
       }
     }
-
+ 
     let playerHand = [];
 
     for (let i = 0; i < 10; i++) {
       let randLetter = letterPool[Math.floor(Math.random() * letterPool.length)];
       playerHand.push(randLetter);
+      letterPool.splice((letterPool.indexOf(randLetter)), 1);
+    }
+
+    for (let letter of playerHand) {
+      letterPool.push(letter);
     }
 
     return playerHand;
@@ -83,6 +88,7 @@ class Adagrams {
   }
   
 }
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
