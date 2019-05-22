5907f4e83c51693a0d9d542a9d59353c5c88b22f
Shubha Rajan <shubharajan94@gmail.com>
2019-05-22 14:21:11 -0700

    fixed formatting for readability

--- src/adagrams.js
+++ src/adagrams.js
@@ -3,7 +3,11 @@ const Adagrams = {
 
   drawLetters() {
     const letters = [];
-    const letter_freq = new Map([['A', 9], ['N', 6], ['B', 2], ['O', 8], ['C', 2], ['P', 2], ['D', 4], ['Q', 1], ['E', 12], ['R', 6], ['F', 2], ['S', 4], ['G', 3], ['T', 6], ['H', 2], ['U', 4], ['I', 9], ['V', 2], ['J',1], ['W', 2], ['K', 1], ['X', 1], ['L', 4], ['Y',2], ['M', 2], ['Z', 1]]);
+    const letter_freq = new Map([
+      ['A', 9], ['N', 6], ['B', 2], ['O', 8], ['C', 2], ['P', 2], ['D', 4], 
+      ['Q', 1], ['E', 12], ['R', 6], ['F', 2], ['S', 4], ['G', 3], ['T', 6], 
+      ['H', 2], ['U', 4], ['I', 9], ['V', 2], ['J',1], ['W', 2], ['K', 1], 
+      ['X', 1], ['L', 4], ['Y',2], ['M', 2], ['Z', 1]]);
     for (let [key, value] of letter_freq) {
         for (let i = 0; i < value; i++) {
               letters.push(key);
@@ -30,7 +34,10 @@ const Adagrams = {
 
   scoreWord(word) {
     const letterScore = new Map([
-      ['A', 1], ['N', 1], ['B',3], ['O',1], ['C', 3], ['P', 3], ['D', 2], ['Q', 10], ['E', 1], ['R', 1], ['F', 4], ['S', 1],['G', 2], ['T', 1], ['H', 4], ['U', 1], ['I', 1], ['V', 4], ['J', 8], ['W', 4], ['K', 5], ['X', 8], ['L', 1], ['Y', 4], ['M', 3], ['Z', 10]
+      ['A', 1], ['N', 1], ['B',3], ['O',1], ['C', 3], ['P', 3], ['D', 2], 
+      ['Q', 10], ['E', 1], ['R', 1], ['F', 4], ['S', 1],['G', 2], ['T', 1], 
+      ['H', 4], ['U', 1], ['I', 1], ['V', 4], ['J', 8], ['W', 4], ['K', 5], 
+      ['X', 8], ['L', 1], ['Y', 4], ['M', 3], ['Z', 10]
     ]);
     let score = 0;
     for (let char of word) {
@@ -53,7 +60,8 @@ const Adagrams = {
       winner = { word: word, score: this.scoreWord(word) };
     } 
     else if (this.scoreWord(word) == winner.score){
-      if (((word.length < winner.word.length) || (word.length == 10)) && (winner.word.length != 10)) {
+      if (((word.length < winner.word.length) || (word.length == 10)) 
+      && (winner.word.length != 10)) {
         winner = { word: word, score: this.scoreWord(word) };
         }
       }
