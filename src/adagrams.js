1669671c576298cc79b4142c5bdcf5fe0bd23719
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-22 14:01:25 -0700

    wrote scoreWord method and it passes tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,6 +28,35 @@ const letterDistribution = {
   "Z": 1,
 }
 
+const letterPoints = {
+  "A": 1,
+  "B": 3,
+  "C": 3,
+  "D": 2,
+  "E": 1,
+  "F": 4,
+  "G": 2,
+  "H": 4,
+  "I": 1,
+  "J": 8,
+  "K": 5,
+  "L": 1,
+  "M": 3,
+  "N": 1,
+  "O": 1,
+  "P": 3,
+  "Q": 10,
+  "R": 1,
+  "S": 1,
+  "T": 1,
+  "U": 1,
+  "V": 4,
+  "W": 4,
+  "X": 8,
+  "Y": 4,
+  "Z": 10,
+}
+
 // const fillPool = (letterPool) => {
 //   Object.keys(letterDistribution).forEach( function(letter) {
 //     for (let i = 0; i < letterDistribution[letter]; i += 1) {
@@ -67,6 +96,18 @@ const Adagrams = {
       }
     }
     return true;
+  },
+
+  scoreWord(word) {
+    let score = 0;
+    word = word.toUpperCase();
+    for (let i = 0; i < word.length; i += 1) {
+      score += letterPoints[word[i]]
+    }
+    if (word.length >= 7) {
+      score += 8;
+    }
+    return score;
   }
 };
 
