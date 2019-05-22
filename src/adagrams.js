b62844433972d2053ad62e90d067dcb0bbb2e457
Shirley Chu <shirley.cchu@gmail.com>
2019-05-22 10:47:09 -0700

    scoreWord function written and tests passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,7 +26,6 @@ const Adagrams = {
     return letterHand
   },
 
-
   usesAvailableLetters (input, lettersinHand) {
     const lettersinHandHash = {};
     lettersinHand.forEach((letter) => {
@@ -47,7 +46,24 @@ const Adagrams = {
     }
 
     return true; 
-  }
+  },
+
+  scoreWord (word) {
+    const letterPoints = {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10};
+    var score = 0;
+    var letter;
+   
+    for (letter in word) {
+      letter = (word[letter]).toUpperCase();
+      score += letterPoints[letter];
+    }
+
+    if(word.length > 6) {
+      score += 8;
+    }
+    
+    return score;
+  },
 };
 
 
