f266791bb81b48f656a97e81e256331ad2d719ea
Margaret Finnan <msfinnan@gmail.com>
2019-05-21 20:03:36 -0700

    added highest score functionality

--- src/adagrams.js
+++ src/adagrams.js
@@ -5,6 +5,7 @@ const Adagrams = {
   drawLetters() {
     const allLetters = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
 
+    //use lodash .sampleSize method to get a random sample of 10 letters 
     const lettersInHand = _.sampleSize(allLetters, 10);
 
     return lettersInHand;
@@ -12,6 +13,7 @@ const Adagrams = {
   },
 
   usesAvailableLetters(input, lettersInHand) {
+    input = input.toUpperCase()
     let lettersObject = {}
     lettersInHand.forEach(function (letter) {
       if (lettersObject[letter] !== undefined) {
@@ -26,13 +28,14 @@ const Adagrams = {
     let passes = true
 
     inputArray.forEach(function (letter) {
-      if (lettersObject[letter] !== undefined ) {
+      if (lettersObject[letter] !== undefined) {
         lettersObject[letter] -= 1;
       } else {
         passes = false;
       }
     });
 
+    //refactor into previous .forEach loop
     Object.values(lettersObject).forEach(function (value) {
       if (value < 0) {
         passes = false;
@@ -44,11 +47,12 @@ const Adagrams = {
   },
 
   scoreWord(word) {
+    word = word.toUpperCase()
 
     const letterScores = {
       'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
       'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
-      'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 
+      'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
       'Y': 4, 'Z': 10
     };
 
@@ -62,15 +66,41 @@ const Adagrams = {
 
     if (wordArray.length > 6) {
       score += 8
-    };
+    }
 
-    return score
+    return score;
   },
 
-  
+  highestScoreFrom(words) {
+
+    const winningWord = {
+      word: "",
+      score: 0,
+    };
+
+    words.forEach(function (word) {
+      if (Adagrams.scoreWord(word) > winningWord['score']) {
+        winningWord['word'] = word;
+        winningWord['score'] = Adagrams.scoreWord(word);
+      } else if (Adagrams.scoreWord(word) === winningWord['score']) {
+        if (word.length === 10 && winningWord['word'].length !== 10) {
+          winningWord['word'] = word;
+          winningWord['score'] = Adagrams.scoreWord(word);
+        } else if (word.length < winningWord['word'].length && winningWord['word'].length !== 10) {
+          winningWord['word'] = word;
+          winningWord['score'] = Adagrams.scoreWord(word);
+        }
+      }
+    });
+
+    return winningWord;
+
+  }
+
+
 };
 
-// Adagrams.scoreWord("XXXXXXXX")
+Adagrams.highestScoreFrom(["XX", "XXX", "XXXXXXXX"])
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
