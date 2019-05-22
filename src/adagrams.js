fe1c72db193665ff92291fc13f8a862e402946fd
Margaret Finnan <msfinnan@gmail.com>
2019-05-22 11:29:20 -0700

    refactored loop in usesAbailableLetters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
-// node -r esm src/adagrams.js 
-// option shift f
+
 const Adagrams = {
   drawLetters() {
     const allLetters = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"];
@@ -27,24 +26,17 @@ const Adagrams = {
 
     const inputArray = input.split("");
 
-    let passes = true;
+    let boolean = true;
 
     inputArray.forEach(function (letter) {
-      if (lettersObject[letter] !== undefined) {
+      if (lettersObject[letter] > 0) {
         lettersObject[letter] -= 1;
       } else {
-        passes = false;
+        boolean = false;
       }
     });
 
-    //refactor into previous .forEach loop
-    Object.values(lettersObject).forEach(function (value) {
-      if (value < 0) {
-        passes = false;
-      }
-    });
-
-    return passes;
+    return boolean;
 
   },
 
