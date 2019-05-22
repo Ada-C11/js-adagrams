db03b300a897a489a59a21ad75118044be888984
Kelly Downes <kedownes@gmail.com>
2019-05-22 14:13:29 -0700

    Add scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -21,6 +21,34 @@ const Adagrams = {
     }
     return true;
   },
+
+  scoreWord(word) {
+    let wordCap = word.toUpperCase();
+    let score = 0;
+    for (const letter in wordCap) {
+      if (['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].includes(wordCap[letter])) {
+        score += 1;
+      } else if (['D', 'G'].includes(wordCap[letter])) {
+        score += 2;
+      } else if (['B', 'C', 'M', 'P'].includes(wordCap[letter])) {
+        score += 3;
+      } else if (['F', 'H', 'V', 'W', 'Y'].includes(wordCap[letter])) {
+        score += 4;
+      } else if (['K'].includes(wordCap[letter])) {
+        score += 5;
+      } else if (['J', 'X'].includes(wordCap[letter])) {
+        score += 8;
+      } else if (['Q', 'Z'].includes(wordCap[letter])) {
+        score += 10;
+      }
+    }
+
+    if (word.length >= 7 && word.length <= 10) {
+      score += 8;
+    }
+
+    return score;
+  },
 };
 
 // Do not remove this line or your tests will break!
