c4374b3dbdb56809a96eaff9e3ee1edb9ba333f7
Shubha Rajan <shubharajan94@gmail.com>
2019-05-22 12:15:30 -0700

    wrote drawLetters and usesAvailableLetters methods

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,32 @@
+const _ = require('lodash');
 const Adagrams = {
+
   drawLetters() {
-    // Implement this method for wave 1
+    const letters = [];
+    const letter_freq = new Map([['A', 9], ['N', 6], ['B', 2], ['O', 8], ['C', 2], ['P', 2], ['D', 4], ['Q', 1], ['E', 12], ['R', 6], ['F', 2], ['S', 4], ['G', 3], ['T', 6], ['H', 2], ['U', 4], ['I', 9], ['V', 2], ['J',1], ['W', 2], ['K', 1], ['X', 1], ['L', 4], ['Y',2], ['M', 2], ['Z', 1]]);
+    for (let [key, value] of letter_freq) {
+        for (let i = 0; i < value; i++) {
+              letters.push(key);
+        }  
+    }
+    return _.sampleSize(letters, 10)
   },
-};
+
+  usesAvailableLetters(input, lettersInHand) {
+    if (input.length > lettersInHand.length){
+      return false;
+    }
+    let possibleLetters = _.clone(lettersInHand);
+    for (let char of input) {
+        if (possibleLetters.includes(char)) {
+          possibleLetters.splice(possibleLetters.indexOf(char), 1);
+        } else {
+          return false;
+        }
+    }
+    return true;
+  },
+}
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
