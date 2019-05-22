3e1a17b4485febcac44201046166a05a92e4c602
amyesh <amyesh08@gmail.com>
2019-05-22 16:09:51 -0700

    added helper method to drawLetters method to generate letter array based on frequency hash

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,33 +2,21 @@ const _ = require("lodash")
 
 const Adagrams = {
   drawLetters() {
-      const letterFrequency = [
-        "A", "A", "A", "A", "A", "A", "A", "A", "A",
-        "B", "B",
-        "C", "C",
-        "D", "D", "D", "D",
-        "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-        "F", "F",
-        "G", "G", "G",
-        "H", "H",
-        "I", "I", "I", "I", "I", "I", "I", "I", "I",
-        "J",
-        "K",
-        "L", "L", "L", "L",
-        "M", "M",
-        "N", "N", "N", "N", "N", "N",
-        "O", "O", "O", "O", "O", "O", "O", "O",
-        "P", "P",
-        "Q",
-        "R", "R", "R", "R", "R", "R",
-        "S", "S", "S", "S",
-        "T", "T", "T", "T", "T", "T",
-        "U", "U", "U", "U",
-        "V", "V",
-        "W", "W", "X",
-        "Y", "Y",
-        "Z",
-      ];
+    const availLetters = {
+      A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12,
+      R: 6, F: 2, S: 4, G: 3, T: 6, H: 2, U: 4, I: 9, V: 2, J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2, M: 2, Z: 1
+    }
+
+    const generateLetters = function generateLetters(availLetters) {
+      let array = []
+      Object.keys(availLetters).forEach( letter => {
+        for (let i = 0; i < availLetters[letter]; i += 1) {
+          array.push(letter);
+        }})
+      return array;
+    }
+
+    const letterFrequency = generateLetters(availLetters);
     const lettersInHand = _.sampleSize(letterFrequency, 10);
     return lettersInHand;
   },
