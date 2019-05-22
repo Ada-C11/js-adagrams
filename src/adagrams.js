3986291895fce87ebc6ff8f902d7094ba2fa00b1
sjscotton <sj.scotton@gmail.com>
2019-05-22 14:29:09 -0700

    all tests pass for Adagrams.highestScoreFrom()

--- src/adagrams.js
+++ src/adagrams.js
@@ -39,38 +39,43 @@ const Adagrams = {
   },
 
   scoreWord(word) {
+    let alphaScores = {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2,
+      H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10,
+      R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10};
     let score = 0;
-    for (let i = 0; i < word.length; i += 1) score += this.letterScores[word[i].toUpperCase()];
+    for (let i = 0; i < word.length; i += 1) score += alphaScores[word[i].toUpperCase()];
     if (word.length >= 7) score += 8;
     return score;
   },
 
   highestScoreFrom(words) {
-    // const scores = [];
-    // for (let i = 0; i < words.length; i += 1) scores[i] = this.scoreWord(words[i]);
 
     const scores = words.map((word) => this.scoreWord(word));
     const maxScore = scores.reduce((a,b) => Math.max(a,b));
     const ties = [];
     let bestWord;
     for (let i = 0; i < words.length; i += 1) {
-      // console.log(this.scoreWord(words[i]));
-      // if (this.scoreWord(words[i]) == maxScore) {
-      //   console.log(words[i]);
-      //   if (words[i].length == 10) {
-      //     bestWord[words[i]] = maxScore;  
-      //   }  else  { 
-      //     ties.push(words[i]); 
-      //   }
-      // }
       if (this.scoreWord(words[i]) == maxScore) {
-        // console.log(words[i]);
-        if (words[i].length == 10) bestWord = words[i];  
-        else ties.push(words[i]);
+        if (words[i].length == 10) {
+           bestWord = words[i]; 
+           break;
+        } else ties.push(words[i]);
       }
     }
     // const shortWord = ties.reduce((a,b) => Math.min(a.length, b.length));
-    if (!bestWord) bestWord = ties.reduce((a,b) => Math.min(a.length, b.length));
+    // console.log(ties);
+    // console.log(scores);
+    // console.log(maxScore);
+    if (!bestWord) {
+      if (ties.length == 1) bestWord = ties[0];
+      else {
+        const minLength = ties.reduce((a,b) => Math.min(a.length, b.length));
+        // console.log(minLength);
+        // console.log(ties);
+        bestWord = ties.filter(word => word.length == minLength)[0];
+        // console.log(bestWord);
+      }
+    }
     const bestWordObj = { word: bestWord, score: maxScore };
     return bestWordObj;
     // console.log(scores);
@@ -82,7 +87,11 @@ const Adagrams = {
 // Adagrams.drawLetters()
 // console.log(Adagrams.drawLetters());
 
+console.log(Adagrams.highestScoreFrom(['MMMM', 'WWW']));
+console.log(Adagrams.highestScoreFrom(['AAAAAAAAAA', 'EEEEEEEEEE']));
 console.log(Adagrams.highestScoreFrom(['AAAAAAAAAA', 'BBBBBB']));
+
+console.log(Adagrams.highestScoreFrom(['X', 'XX', 'XXX', 'XXXX', 'XXXX']));
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
