4bd46a41633e3bf2787146068baa09d9c14a86a0
Mina Shin <minamishin@hotmail.com>
2019-05-22 15:54:41 -0700

    added usesAvailableLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -6,19 +6,38 @@ const Adagrams = {
 
   let myLetters = [];
   let randomLetters = poolOfLetters.slice();
-
-  let randomIndex = Math.floor(Math.random() * (poolOfLetters.length));
-
+  
     for (let i = 0; i < 10; i += 1) {
+        const randomIndex = Math.floor(Math.random() * (randomLetters.length));
         myLetters.push(randomLetters[randomIndex]);
         randomLetters.splice(randomIndex, 1);
     }
-
     return myLetters;
   },
-};
 
+  usesAvailableLetters(input, lettersInHand) {
+    if (input.length <= 10) {
+      for (let i = 0; i < input.length; i += 1) {
+        let letter = input.charAt(i);
+        if (!lettersInHand.includes(letter)) {
+          return false;
+        }
+        let letterIndex = lettersInHand.indexOf(letter);
+        lettersInHand.splice(letterIndex, 1);
+      }
+      return true;
+    }
+  }
+}
+//       word.forEach(function(eachLetterInWord) {
+//         if (!lettersInHand.include(eachLetterInWord)) {
+//             return false
+//         }
+//         lettersInHand.delete_at(lettersInHand.index(each_letter_in_word))      
+//       } return true
+//         return false    
+// };
 // console.log(Adagrams.drawLetters());
-
+// console.log(Adagrams.usesAvailableLetters());
 // Do not remove this line or your tests will break!
 export default Adagrams;
