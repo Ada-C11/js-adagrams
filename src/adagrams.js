df32a1c1248781f68adbdd6cce96f350e2260380
Cloudy Lopez <cloudylopez@gmail.com>
2019-05-22 18:44:45 -0700

    fixed the method to properly shuffle. deleted lines that werent doing anything.

--- src/adagrams.js
+++ src/adagrams.js
@@ -4,14 +4,16 @@ const Adagrams = {
       "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"
     ];
 
-    let thisHand = poolOfLetters.slice();
+    // let thisHand = poolOfLetters.slice();
+
     let myLetters = [];
-    let shuffleLetters = Math.floor(Math.random() * thisHand.length);
     for (let i = 0; i < 10; i +=1) {
-      myLetters.push(thisHand[shuffleLetters]);
-      thisHand.splice( shuffleLetters, 1 );
+      let randomIndexNum = Math.floor(Math.random() * poolOfLetters.length);
+      myLetters.push(poolOfLetters[randomIndexNum]);    
+      // console.log(randomIndexNum, myLetters, poolOfLetters[randomIndexNum])
+
     }
-    return myLetters;
+        return myLetters;
   //  console.log(myLetters);
   },
 } 
