d8721aa4d3ff64be4e9039a5b8d6d2b2a43408f3
Hana <hanalways@gmail.com>
2019-05-23 06:47:42 -0700

    refactored drawLetters with shuffle func and slice

--- src/adagrams.js
+++ src/adagrams.js
@@ -10,10 +10,19 @@ const Adagrams = {
       }
     });
 
-    let drawnLetters = []
-    for (let i = 0; i < 10; i += 1) {
-      drawnLetters[i] = letterPool[Math.floor(Math.random()*letterPool.length)];
+    // let drawnLetters = []
+    // for (let i = 0; i < 10; i += 1) {
+    //   drawnLetters[i] = letterPool[Math.floor(Math.random()*letterPool.length)];
+    // }
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i -= 1) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
     }
+
+    shuffle(letterPool);
+    let drawnLetters = letterPool.slice(0, 10);
 
     return drawnLetters;
   },
@@ -22,12 +31,28 @@ const Adagrams = {
     let size = 0;
     let inputArray = input.toUpperCase().split('');
 
-  
-  }
+    // lettersInHand = 
+
+    // lettersInHand into a hash
+    // iterate through input to lookup char in hash
+    // return true if all have been looked up
+  },
+
+  scoreWord(word) { 
+    // hash object with point value
+    // initialize score = 0
+    // iterate through word
+    // increment score with each value
+    // check length
+
+    // return int (score)
+  },
+
+
 };
 
-console.log(Adagrams.drawLetters());
+// console.log(Adagrams.drawLetters());
 // console.log(Adagrams.usesAvailableLetters('dog', lettersInHand));
 
 // Do not remove this line or your tests will break!
-// export default Adagrams;
+export default Adagrams;
