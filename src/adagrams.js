2a849e21183fd68e0491046837c5cd45e0b3e8b5
Kate Sandler <kate@katekatekate.net>
2019-05-24 10:34:23 -0700

    Add highest score method

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,6 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
-    const letterBank = {
+    const letter_bank = {
       A: 9,
       B: 2,
       C: 2,
@@ -30,34 +29,110 @@ const Adagrams = {
       Z: 1
     };
 
-    let myLetters = [];
+    let letters = [];
 
-    for (let key in letterBank) {
-      while (letterBank[key] > 0) {
-        myLetters.push(key);
-        letterBank[key] -= 1;
+    for (let key in letter_bank) {
+      while (letter_bank[key] > 0) {
+        letters.push(key);
+        letter_bank[key] -= 1;
       }
     }
 
-    myLetters.sort(() => Math.random());
-    return myLetters.slice(0, 10);
+    letters.sort(() => Math.random() - 0.5);
+    return letters.slice(0, 10);
   },
 
-  usesAvailableLetters(word, drawn) {
-    const handMap = {};
-    drawn.forEach(letter => {
-      if (handMap[letter]) handMap[letter] += 1;
-      else handMap[letter] = 1;
+  usesAvailableLetters(input, lettersInHand) {
+    const lettersInHandMap = {};
+
+    lettersInHand.forEach(letter => {
+      if (lettersInHandMap[letter]) {
+        lettersInHandMap[letter] += 1;
+      } else {
+        lettersInHandMap[letter] = 1;
+      }
     });
-    console.log(handMap);
-    //use a for loop (if/else statement) to make sure they have all the letters in input and if they do, decrement the value of the letter so that they've spent it
-    for (let i = 0; i < word.length; i += 1) {
-      if (hand[word[i]]) hand[word[i]] -= 1;
-      else return false;
+
+    for (let i = 0; i < input.length; i++) {
+      if (lettersInHandMap[input[i]]) {
+        lettersInHandMap[input[i]] -= 1;
+      } else {
+        return false;
+      }
     }
     return true;
+  },
+
+  scoreWord(word) {
+    const letter_value = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    };
+
+    let value = 0;
+
+    for (let i = 0; i < word.length; i++) {
+      let upcaseLetter = word.toUpperCase();
+      value += letter_value[upcaseLetter[i]];
+    }
+
+    if (word.length > 6) {
+      value += 8;
+    }
+    return value;
+  },
+
+  // wave 4
+  highestScoreFrom(words) {
+    let highScore = {
+      word: '',
+      score: 0
+    };
+
+    words.forEach(word => {
+      const score = this.scoreWord(word);
+
+      if (score > highScore.score) {
+        highScore.score = score;
+        highScore.word = word;
+      } else if (score == highScore.score) {
+        if (word.length == 10 && highScore.word.length != 10) {
+          highScore.word = word;
+        } else if (
+          word.length < highScore.word.length &&
+          highScore.word.length != 10
+        ) {
+          highScore.word = word;
+        }
+      }
+    });
+
+    return highScore;
   }
 };
 
-// Do not remove this line or your tests will break!
 export default Adagrams;
