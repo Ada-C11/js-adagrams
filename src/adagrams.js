2c4cf4cc1541bd1065e86bf132f1ab1d2255deb2
Ngoc Le <ngocle@outlook.com>
2019-05-23 17:06:34 -0700

    added highestScoreForm method and debugged

--- src/adagrams.js
+++ src/adagrams.js
@@ -21,18 +21,16 @@ const Adagrams = {
       }
     }
 
-    //Draw random letters
     let randomLetters = [];
-    let bankLength = letterBank.length;
     let letterBankCopy = letterBank.slice();
 
     for ( let i = 0; i < 10; i += 1 ) {
-      let randIndex = Math.floor(Math.random() * bankLength);
+      let randIndex = Math.floor(Math.random() * letterBankCopy.length);
       
       randomLetters.push(letterBankCopy[randIndex]);
       letterBankCopy.splice(randIndex, 1);
-    }
-   return randomLetters;
+    } 
+    return randomLetters;
   },
 
   usesAvailableLetters(input,lettersInHand) {
@@ -56,8 +54,80 @@ const Adagrams = {
         return false;
     }
   },
-}
 
+  scoreWord(word) {
+    let score = 0;
+    const letterScore = { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2, 
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+    };
+
+    let letterArray = word.toUpperCase().split('');
+
+    letterArray.forEach(char => {
+      score += letterScore[char];
+    });
+
+    if (letterArray.length > 6) {
+        score += 8;
+    }
+
+    return score
+
+  },
+
+  highestScoreFrom(stringArray) {
+    let scoresArray = []
+    let winningWordArray = []
+    stringArray.forEach(string => {
+      scoresArray.push(Adagrams.scoreWord(string))
+    });
+    
+    let i = 0
+    let maxScore = scoresArray[0]
+   
+    while (i < scoresArray.length) {
+      if (maxScore < scoresArray[i]) {
+        maxScore = scoresArray[i];
+      }
+      i += 1;
+    }
+    
+    let j = 0
+    while (j < scoresArray.length) {
+      if (scoresArray[j] == maxScore) {
+        winningWordArray.push(stringArray[j]);
+      }
+      j += 1;
+    }
+
+    let winningWord = winningWordArray[0];
+    let wordMinLength = winningWordArray[0].length;
+
+    for (let n = winningWordArray.length - 1; n >= 0; n -= 1) {
+      if (winningWordArray[n].length == 10) {
+        winningWord = winningWordArray[n];
+        continue;
+      }
+      else if (wordMinLength > winningWordArray[n].length) {
+        wordMinLength = winningWordArray[n].length;
+        winningWord = winningWordArray[n];
+        } 
+    }
+  
+    let winningWordHash = {
+      word: winningWord,
+      score: maxScore,
+    }
+    return winningWordHash
+
+  }  
+    
+};
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
