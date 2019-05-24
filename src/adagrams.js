89629d8f1a67a2e28d66fea8a8a191cf1df6335c
Shamira <marshallscm@gmail.com>
2019-05-24 16:50:55 -0700

    Added  point values to words.

--- src/adagrams.js
+++ src/adagrams.js
@@ -47,40 +47,25 @@ usesAvailableLetters(input, lettersInHand) {
   },
 
 scoreWord(word) {
-  let pointValue = [
-    {
-      letters: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-      values: 1,
-    }, 
-    {
-      letters: ["D", "G"],
-      values: 2,
-    },
-    {
-      letters: ["B", "C", "M", "P"],
-      values: 3,
-    },
-    {
-      letters: ["F", "H", "V", "W", "Y"],
-      values: 4,
-    },
-    {
-      letters: ["K"],
-      values: 5,
-    },
-    {
-      letters: ["J", "X"],
-      values: 8,
-    },
-    {
-      letters: ["Q", "Z"],
-      values: 10,
-    },
-  ]
+  let pointValue = {
+      'A': 1,'E': 1,'I': 1,'O': 1,'U': 1,'L': 1,'N': 1,
+      'R': 1,'S': 1,'T': 1,'D': 2,'G': 2,'B': 3,'C': 3,
+      'M': 3,'P': 3,'F': 4,'H': 4,'V': 4,'W': 4,'Y': 4,
+      'K': 5,'J': 8,'X': 8,'Q': 10,'Z': 10 
+    };
+
+let wordScore = 0;
+word = word.toUpperCase().split('');
+ 
+word.forEach(function(letter){
+  wordScore += pointValue[letter];
+});
+
+ 
+},
+
 
 }
-  
-};
 // console.log(Adagrams.drawLetters());
 // Do not remove this line or your tests will break!
 export default Adagrams;
