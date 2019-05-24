c29445296df99824ed8eedb4d83f05a0c557a40a
Cloudy Lopez <cloudylopez@gmail.com>
2019-05-23 17:37:55 -0700

    skipped tests not needed, created the letterScore array and added the logic or scoring a word

--- src/adagrams.js
+++ src/adagrams.js
@@ -11,15 +11,36 @@ const Adagrams = {
         return myLetters;
   },
 
-  usesAvailableLetters(input, myLetters) {
+  usesAvailableLetters(input, lettersInHand) {
     for (let i = 0; i < input.length; i +=1) {
-      const char = input.charAt(i;)
-      if (!lettersInHand.include?(char)) {
+      const char = input.charAt(i);
+      if (!lettersInHand.includes(char)) {
         return false;
       }
+      const index = lettersInHand.indexOf(char);
+      lettersInHand.splice(index, 1);
     }
-  }
-console.log(Adagrams.drawLetters());
-// Do not remove this line or your tests will break!
-// export default Adagrams;
+    return true;
+  },
 
+  scoreWord(word) {
+    const letterScore = { 
+      "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2,"H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1,"O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10,
+    }
+    let wordToScore = word.toUpperCase().split(''); //ln 36
+
+    let points = 0; 
+    if(wordToScore.length >= 7) {
+      points += 8;
+    }
+    wordToScore.forEach( function(char) {
+      points += letterScore[char];
+    }
+  )
+  return points;
+  }
+}
+
+// console.log(Adagrams.drawLetters());
+// Do not remove this line or your tests will break!
+export default Adagrams;
