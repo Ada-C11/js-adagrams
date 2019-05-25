d8947180b95e0b72d162667c5ff6a36f854ed3bb
Amy Phung <phung.amy85@gmail.com>
2019-05-25 15:05:06 -0700

    remove comments, clean up white space,  check vars

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,21 +1,17 @@
 const _ = require('lodash')
 
 const letterPool = {
-  'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2,
-  'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1,
+  'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3,
+  'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6,
+  'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
+  'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1,
 }
 
 const Adagrams = {
-  // return the number of times a single (case insensitive) char occurs inside the container it's in
-  // letterCount(char, container) {
-  //   return (container.match(/char/gi) || []).length
-  // },
-
-
   drawLetters() {
     const keys = Object.keys(letterPool);
     const length = keys.length;
-    let lettersInHand = [];
+    const lettersInHand = [];
 
     for (let i = 0;i < 10;i += 1) {
       const randomIndex = Math.floor(Math.random() * length);
@@ -26,15 +22,14 @@ const Adagrams = {
   },
 
   usesAvailableLetters(input, lettersInHand) {
-    let duplicateLettersInHand = lettersInHand.sort();
-    const sortedInputArray = input.split('');
+    const duplicateHand = lettersInHand.sort();
+    const inputArray = input.split('');
 
-    let array = []
+    const comparisonArray = []
     for (let letter of input) {
-      array.push((duplicateLettersInHand.splice(duplicateLettersInHand.indexOf(letter), 1).toString()))
+      comparisonArray.push((duplicateHand.splice(duplicateHand.indexOf(letter), 1).toString()))
     }
-
-    if (_.isEqual(array, sortedInputArray)) {
+    if (_.isEqual(comparisonArray, inputArray)) {
       return true;
     } else {
       return false;
@@ -44,7 +39,10 @@ const Adagrams = {
 
   scoreWord(word) {
     const scoreChart = {
-      'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10,
+      'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2,
+      'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1,
+      'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
+      'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10,
     };
 
     const upperCaseWord = word.toUpperCase();
@@ -62,7 +60,6 @@ const Adagrams = {
   },
 
   highestScoreFrom(words) {
-
     const allScores = words.map((word) => {
       const newWord = {
         'word': word,
@@ -71,19 +68,19 @@ const Adagrams = {
       return newWord;
     });
 
-
-    // iterate over allScores to find the highest score
-    // make the highestScore k/v pair from that
-
     let winningWord = allScores[0];
     for (let i in allScores) {
       if (allScores[i].score > winningWord.score) {
         winningWord.word = allScores[i].word;
         winningWord.score = allScores[i].score;
-      } else if (allScores[i].score === winningWord.score && allScores[i].word.length === 10) {
+      } else if
+        (allScores[i].score === winningWord.score &&
+        allScores[i].word.length === 10) {
         winningWord.word = allScores[i].word;
         return winningWord;
-      } else if (allScores[i].score === winningWord.score && allScores[i].word.length < winningWord.word.length) {
+      } else if
+        (allScores[i].score === winningWord.score &&
+        allScores[i].word.length < winningWord.word.length) {
         winningWord.word = allScores[i].word;
         return winningWord;
       }
@@ -91,7 +88,6 @@ const Adagrams = {
     return winningWord;
   }
 }
-
 
 
 //Do not remove this line or your tests will break!
