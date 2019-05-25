6e80d9a66f5acf098115b222e514365abc7d216f
Mina Shin <minamishin@hotmail.com>
2019-05-24 18:47:14 -0700

    created function to return the winner with the highest score

--- src/adagrams.js
+++ src/adagrams.js
@@ -16,7 +16,7 @@ const Adagrams = {
   },
 
   usesAvailableLetters(input, lettersInHand) {
-    let splitInput = input.split('');
+    const splitInput = input.split('');
     if (splitInput.length <= 10) {
       for (let i = 0; i < splitInput.length; i += 1) {
         if (!lettersInHand.includes(splitInput[i])) {
@@ -27,11 +27,83 @@ const Adagrams = {
       }
      return true;
     }
+  },
+
+  scoreWord(word) {
+    const letterPoints = {
+      'A': 1,
+      'B': 3,
+      'C': 3,
+      'D':2, 
+      'E':1, 
+      'F':4, 
+      'G':2, 
+      'H':4, 
+      'I':1, 
+      'J':8, 
+      'K':5, 
+      'L':1, 
+      'M':3, 
+      'N':1, 
+      'O':1, 
+      'P':3, 
+      'Q':10, 
+      'R':1, 
+      'S':1, 
+      'T':1, 
+      'U':1, 
+      'V':4, 
+      'W':4, 
+      'X':8, 
+      'Y':4, 
+      'Z':10,
+    };
+
+    let myScore = 0;
+
+    if (word === '') {
+      return myScore;
+    }
+
+    let wordToScore = word.toUpperCase().split('');
+    let scoreMap = wordToScore.map(eachLetter => letterPoints[eachLetter]);
+    const scoring = (totalScore, letterValue) => totalScore + letterValue;
+ 
+    myScore = scoreMap.reduce(scoring);
+
+    if (wordToScore.length > 6) {
+      return myScore += 8;
+    }
+    return myScore;
+  },
+
+  highestScoreFrom(words) {
+  let winner = {
+    word: '',
+    score: 0,
+  }
+
+  words.forEach((word) => {
+    const currentScore = this.scoreWord(word);
+
+    if (currentScore > winner.score) {
+      winner.score = currentScore;
+      winner.word = word;
+    } else if (currentScore === winner.score) {
+      if ((word.length === 10 && winner.word.length != 10) ||
+      (word.length < winner.word.length && winner.word.length != 10)) {
+        winner.score = currentScore;
+        winner.word = word;
+      }
+    }
+  });
+
+  return winner;
   }
 }
 
 
 // console.log(Adagrams.drawLetters());
-// console.log(Adagrams.usesAvailableLetters());
+// console.log(Adagrams.scoreWord(''));
 // Do not remove this line or your tests will break!
 export default Adagrams;
