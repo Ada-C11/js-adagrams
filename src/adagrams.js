491efe2f99495a73d2467305ee4936e975289f73
Shamira <marshallscm@gmail.com>
2019-05-25 14:24:50 -0700

    Added logic for highestScoreFrom(words). Finished wave 4.

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,8 +2,9 @@ import { arrayExpression } from "@babel/types";
 
 const Adagrams = {
  
-  drawLetters() {
-    const letterPool = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K:1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
+drawLetters() {
+    const letterPool = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K:1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, 
+      T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
     
       const letterArray = [] 
       // call forEach on entries and add it to letterArray
@@ -68,16 +69,37 @@ if (word.length > 6){
 } 
 },
 
+
 highestScoreFrom(words){
-  let winningWord = {
-    word: '',
-    score: 0
-  };
 
-  // 
-return winningWord;
+let winner = {
+  'word': '',
+  'score': 0,
+};
+
+// Question for Dee. There has to be a more suscinct way of doing all of these conditionals, but I couldn't thing of one. 
+// Would like to talk about it in our one on one.  
+// learned about this for...of statement.  
+
+for (let word of words) {
+  if (word.length == 10 && winner['word'].length != 10) {
+    winner['word'] = word;
+    winner['score'] = this.scoreWord(word);
+  } else if (this.scoreWord(word) >= winner['score'] && word.length == 10 && winner['word'].length != 10) {
+    winner['word'] = word;
+    winner['score'] = this.scoreWord(word);
+  } else if (this.scoreWord(word) >= winner['score'] && word.length < winner['word'].length && winner['word'].length != 10) {
+    winner['word'] = word;
+    winner['score'] = this.scoreWord(word);
+  } else if (this.scoreWord(word) > winner['score']) {
+    winner['word'] = word;
+    winner['score'] = this.scoreWord(word);
+  }
+}
+
+return winner;
+
 },
-
 }
 // console.log(Adagrams.drawLetters());
 // Do not remove this line or your tests will break!
